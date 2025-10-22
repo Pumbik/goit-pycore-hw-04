5901187462b5526d67769653e568ade42af02cf7
Oleksii Kocherhin <Oleksiy.Kochergin@lime-systems.com>
2025-10-23 00:50:49 +0300

    Add task_1

---
+++ task_1/main.py
@@ -0,0 +1,39 @@
+
+def total_salary(path) -> tuple:
+    total = 0
+    average = 0
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            lines =[el.strip() for el in file.readlines()]
+            count_emp = len(lines)
+
+            for line in lines:
+                try:
+                    name, salary = line.split(",")
+                    total += float(salary)
+                except ValueError:
+                    print(f"Некоректний формат рядка: '{line}'. Пропускаємо цей рядок.")
+                    count_emp -= 1
+                    continue
+
+            if count_emp == 0:
+                print("Помилка: Файл порожній, неможливо розрахувати середнє.")
+                return (0, 0)
+
+            average = total / count_emp
+            return total, average
+
+    except FileNotFoundError:
+        print(f"Помилка: Файл за шляхом '{path}' не знайдено.")
+        return (0, 0)
+
+
+
+
+path = "task_1\employees_salary.txt"
+
+total, average = total_salary(path)
+print(f"Загальна сума заробітної плати: ${total}, Середня заробітна плата: ${average}")
+
+
